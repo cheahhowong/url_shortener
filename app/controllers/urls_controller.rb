2fb7236de59cdf862fcc105776b4b809abbe72f0
cheahhowong <cheahhowong@hotmail.com>
2017-12-27 11:56:00 +0800

    add shorten_url function

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -7,4 +7,24 @@ class UrlsController < ApplicationController
 		@url = Url.find(params[:id])
 	end
 
+	def new
+		@url = Url.new
+	end
+
+	def create
+		@url = Url.new(url_params)
+		@url.shorten_url
+		if @url.save
+			redirect_to @url
+		else
+			render 'new'
+			byebug
+		end
+	end
+
+	private
+	def url_params
+		params.require(:url).permit(:long_url)
+	end
+
 end
