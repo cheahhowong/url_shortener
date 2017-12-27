16350182647e866e2ab11eda5bb5fcae8c7a7a16
cheahhowong <cheahhowong@hotmail.com>
2017-12-27 11:25:39 +0800

    create validation for class Url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,7 @@
-class Url < ApplicationRecord
+class Url < ApplicationRecord		
+	validates :long_url, format: { with: /(http:\/\/)?(https:\/\/)?www.+/, message: "Please enter valid URL"}
+
+	def shorten_url
+		self.short_url = SecureRandom.hex(4)
+	end
 end
